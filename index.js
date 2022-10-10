4ec56080bd5e1e1f241e558a6e9554cae068c6de
fortu038 <fortu038@umn.edu>
2022-10-10 15:26:37 -0500

    modified index.js and generateMarkdown.js

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is your project's name?",
-    "Enter a project description.",
-    "Enter installation instructions.",
-    "Enter usage instructions.",
-    "Enter contribution guidelines.",
-    "Enter testing instructions."
+    "What is your project's name?", // 0
+    "Enter a project description.", // 1
+    "Enter installation instructions.", // 2
+    "Enter usage instructions.", // 3
+    "Enter any credits.", // 4
+    "Enter your license.", // 5
+    "Enter contribution guidelines.", // 6
+    "Enter testing instructions." // 7
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    let license = generateMarkdown.renderLicenseSection(data[5]);
-    fs.writeFile(`${fileName}.txt`,
-    `#${fileName}
+    console.log(data.license);
+    let licenseText = generateMarkdown.renderLicenseSection(data.license);
+    fs.writeFile(`${fileName}-readme.md`,
+    `# ${fileName}
     
-    ##Description
+    ## Description
     
-    ${data[1]}
+    ${data.description}
 
     ## Table of Contents
 
@@ -30,29 +33,25 @@ function writeToFile(fileName, data) {
     - [Credits](#credits)
     - [License](#license)
     
-    ##Installation
+    ## Installation
     
-    ${data[2]}
+    ${data.install}
     
-    ##Usage
+    ## Usage
     
-    ${data[3]}
+    ${data.usage}
     
     ##Credits
     
-    ${data[4]}
-    
-    ##License
-    
-    ${license}
-    
+    ${data.credits}  
+    ${licenseText}
     ## How to Contribute
 
-    ${data[5]}
+    ${data.contribute}
 
     ## Tests
 
-    ${data[6]}`,
+    ${data.tests}`,
     (err) => err ? console.error(err) : console.log('Success!'))
 }
 
@@ -82,17 +81,65 @@ function init() {
             },
             {
                 input: "input",
-                name: "contribution",
+                name: "credits",
                 message: questions[4],
             },
             {
-                input: "input",
-                name: "testing",
+                type: "list",
+                name: "license",
                 message: questions[5],
+                choices: [
+                    "No License",
+                    "Academic Free License v3.0",
+                    "Apache license 2.0",
+                    "Artistic license 2.0",
+                    "Boost Software License 1.0",
+                    "BSD 2-clause 'Simplified' license",
+                    "BSD 3-clause 'New' or 'Revised' license",
+                    "BSD 3-clause Clear license",
+                    "Creative Commons license family",
+                    "Creative Commons Zero v1.0 Universal",
+                    "Creative Commons Attribution 4.0",
+                    "Creative Commons Attribution Share Alike 4.0",
+                    "Do What The F*ck You Want To Public License",
+                    "Educational Community License v2.0",
+                    "Eclipse Public License 1.0",
+                    "Eclipse Public License 2.0",
+                    "European Union Public License 1.1",
+                    "GNU Affero General Public License v3.0",
+                    "GNU General Public License family",
+                    "GNU General Public License v2.0",
+                    "GNU General Public License v3.0",
+                    "GNU Lesser General Public License family",
+                    "GNU Lesser General Public License v2.1",
+                    "GNU Lesser General Public License v3.0",
+                    "ISC",
+                    "LaTeX Project Public License v1.3c",
+                    "Microsoft Public License",
+                    "MIT",
+                    "Mozilla Public License 2.0",
+                    "Open Software License 3.0",
+                    "PostgreSQL License",
+                    "SIL Open Font License 1.1",
+                    "University of Illinois/NCSA Open Source License",
+                    "The Unlicense",
+                    "zLib License"
+                ]
+            },
+            {
+                input: "input",
+                name: "contribute",
+                message: questions[6],
+            },
+            {
+                input: "input",
+                name: "tests",
+                message: questions[7],
             },
         ])
         .then((answers) => {
-            writeToFile(answers[0], answers);
+            console.log(answers);
+            writeToFile(answers.name, answers);
         })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,42 @@
+// Dictionary that stores the full license name as a key and the license shorthand as a value
+const licenseDictionary = {
+  "No License" : "",
+  "Academic Free License v3.0" : "afl-3.0",
+  "Apache license 2.0" : "apache-2.0",
+  "Artistic license 2.0" : "artistic-2.0",
+  "Boost Software License 1.0" : "bsl-1.0",
+  "BSD 2-clause 'Simplified' license" :	"bsd-2-clause",
+  "BSD 3-clause 'New' or 'Revised' license" : "bsd-3-clause",
+  "BSD 3-clause Clear license" : "bsd-3-clause-clear",
+  "Creative Commons license family" : "cc",
+  "Creative Commons Zero v1.0 Universal" : "cc0-1.0",
+  "Creative Commons Attribution 4.0" : "cc-by-4.0",
+  "Creative Commons Attribution Share Alike 4.0" : "cc-by-sa-4.0",
+  "Do What The F*ck You Want To Public License" : "wtfpl",
+  "Educational Community License v2.0" : "ecl-2.0",
+  "Eclipse Public License 1.0" : "epl-1.0",
+  "Eclipse Public License 2.0" : "epl-2.0",
+  "European Union Public License 1.1" :	"eupl-1.1",
+  "GNU Affero General Public License v3.0" : "agpl-3.0",
+  "GNU General Public License family" : "gpl",
+  "GNU General Public License v2.0" : "gpl-2.0",
+  "GNU General Public License v3.0" : "gpl-3.0",
+  "GNU Lesser General Public License family" : "lgpl",
+  "GNU Lesser General Public License v2.1" : "lgpl-2.1",
+  "GNU Lesser General Public License v3.0" : "lgpl-3.0",
+  "ISC" : "isc",
+  "LaTeX Project Public License v1.3c" : "lppl-1.3c",
+  "Microsoft Public License" : "ms-pl",
+  "MIT" : "mit",
+  "Mozilla Public License 2.0": "mpl-2.0",
+  "Open Software License 3.0" : "osl-3.0",
+  "PostgreSQL License" : "postgresql",
+  "SIL Open Font License 1.1" : "ofl-1.1",
+  "University of Illinois/NCSA Open Source License" : "ncsa",
+  "The Unlicense" : "unlicense",
+  "zLib License" : "zlib",
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
@@ -19,18 +58,23 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license == "") {
-    return "";
+  let shorthand = licenseDictionary.license;
+  if(shorthand == "") {
+    return "\n";
   }
-  let badge = renderLicenseBadge(license);
-  let link = renderLicenseLink(license);
-  return `${badge}
-  License Link: ${link}`;
+  let markdown = generateMarkdown("License")
+  let badge = renderLicenseBadge(shorthand);
+  let link = renderLicenseLink(shorthand);
+  return `
+  ${markdown}
+  ${badge}
+  License Link: ${link}
+  `;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `##${data.title}`;
+  return `## ${data}`;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown.js;
